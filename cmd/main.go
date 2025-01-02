82a47ef0210e470264ea092f4fb0755402e9c1bf
gula <git@gula.dev>
2025-01-02 19:45:27 +0100

    refactor recalculate

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/goburrow/modbus"
@@ -15,9 +16,11 @@ import (
 )
 
 var (
-	host string
-	port string
-	path string
+	host     string
+	port     string
+	path     string
+	interval string
+	prefix   string
 )
 
 type Register struct {
@@ -26,86 +29,97 @@ type Register struct {
 	Unit    string
 	Name    string
 	Desc    string
-	Factor  float32
+	Factor  float64
 	Address uint16
 }
 
-const prefix = "nibe"
-
-var registers = [6]Register{
-	{Address: 1, Type: "u16", Unit: "°C", Factor: 10, Mode: "r", Desc: "Outdoor temperature", Name: fmt.Sprintf("%s_outdoor_temperature", prefix)},
-	{Address: 5, Type: "u16", Unit: "°C", Factor: 10, Mode: "r", Desc: "Supply temperature", Name: fmt.Sprintf("%s_supply_temperature", prefix)},
-	{Address: 7, Type: "u16", Unit: "°C", Factor: 10, Mode: "r", Desc: "Return temperature", Name: fmt.Sprintf("%s_return_temperature", prefix)},
-	{Address: 8, Type: "u16", Unit: "°C", Factor: 10, Mode: "r", Desc: "Hot water top", Name: fmt.Sprintf("%s_hotwater_temperature", prefix)},
-	{Address: 9, Type: "u16", Unit: "°C", Factor: 10, Mode: "r", Desc: "Hot water charging", Name: fmt.Sprintf("%s_hotwater_charging", prefix)},
-	{Address: 2166, Type: "u32", Unit: "W", Factor: 10, Mode: "r", Desc: "Power usage", Name: fmt.Sprintf("%s_power_usage", prefix)},
+func setName(suffix string) string {
+	return fmt.Sprintf("%s_%s", prefix, suffix)
 }
 
-var outdoorTemperature = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "nibe_outdoor_temperature",
-	Help: "Outdoor temperature",
-})
-
-var supplyTemperature = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "nibe_supply_temperature",
-	Help: "Supply temperature",
-})
-
-var returnTemperature = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "nibe_return_temperature",
-	Help: "Return temperature",
-})
-
-var hotWaterTemperature = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "nibe_hotwater_temperature",
-	Help: "Hot water temperature",
-})
-
-var hotWaterCharging = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "nibe_hotwater_charging",
-	Help: "Hot water charging",
-})
-
-var powerUsage = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "nibe_power_usage",
-	Help: "Power usage",
-})
-
 func getData(client modbus.Client, reg Register) float64 {
-	result, err := client.ReadInputRegisters(reg.Address, 1)
+	var result []byte
+	var err error
+
+	switch reg.Type {
+	case "u16", "s16":
+		result, err = client.ReadInputRegisters(reg.Address, 1)
+	case "u32":
+		result, err = client.ReadInputRegisters(reg.Address, 2)
+	default:
+		panic("unsupported register type")
+	}
+
 	if err != nil {
 		panic(err)
 	}
-	return float64(binary.BigEndian.Uint16(result)) / float64(reg.Factor)
+
+	var value float64
+	switch reg.Type {
+	case "u16":
+		value = float64(binary.BigEndian.Uint16(result))
+	case "s16":
+		value = float64(int16(binary.BigEndian.Uint16(result)))
+	case "u32":
+		value = float64(binary.BigEndian.Uint32(result))
+	}
+
+	return value / reg.Factor
 }
 
-func recordMetrics(client modbus.Client) {
-	go func() {
-		for {
-			outdoorTemperature.Set(getData(client, registers[0]))
-			supplyTemperature.Set(getData(client, registers[1]))
-			returnTemperature.Set(getData(client, registers[2]))
-			hotWaterTemperature.Set(getData(client, registers[3]))
-			hotWaterCharging.Set(getData(client, registers[4]))
-			time.Sleep(30 * time.Second)
+func recordMetrics(client modbus.Client, registers []Register, metrics map[string]prometheus.Gauge) {
+	duration, err := strconv.Atoi(interval)
+	if err != nil {
+		slog.Error("Could not convert interval to int: %s", err)
+	}
+	ticker := time.NewTicker(time.Duration(duration) * time.Second)
+	defer ticker.Stop()
+
+	for {
+		for _, reg := range registers {
+			if gauge, ok := metrics[reg.Name]; ok {
+				gauge.Set(getData(client, reg))
+			} else {
+				slog.Error("Metric not found for register: %s", reg.Name)
+			}
 		}
-	}()
+		<-ticker.C
+	}
 }
 
 func init() {
 	flag.StringVar(&host, "host", "0.0.0.0", "Heat pump host, assumes port 502")
 	flag.StringVar(&port, "port", "2112", "Metrics port, default 2112")
 	flag.StringVar(&path, "path", "/metrics", "Metrics path, default /metrics")
+	flag.StringVar(&interval, "interval", "30", "Fetch interval, default 30s")
+	flag.StringVar(&prefix, "prefix", "nibe", "Prefix for metrics, default nibe")
+
+	flag.Parse()
 }
 
 func main() {
-	flag.Parse()
-
 	resourceAddress := fmt.Sprintf("%s:502", host)
 	servePort := fmt.Sprintf(":%s", port)
 
+	registers := []Register{
+		{Address: 1, Type: "s16", Unit: "°C", Factor: 10, Mode: "r", Desc: "Outdoor temperature in °C", Name: "outdoor_temperature"},
+		{Address: 5, Type: "s16", Unit: "°C", Factor: 10, Mode: "r", Desc: "Supply temperature in °C", Name: "supply_temperature"},
+		{Address: 7, Type: "s16", Unit: "°C", Factor: 10, Mode: "r", Desc: "Return temperature in °C", Name: "return_temperature"},
+		{Address: 8, Type: "u16", Unit: "°C", Factor: 10, Mode: "r", Desc: "Hot water top in °C", Name: "hotwater_temperature"},
+		{Address: 9, Type: "u16", Unit: "°C", Factor: 10, Mode: "r", Desc: "Hot water charging in °C", Name: "hotwater_charging"},
+		{Address: 2166, Type: "u32", Unit: "W", Factor: 10, Mode: "r", Desc: "Power usage in Watt", Name: "power_usage"},
+	}
+
+	metrics := make(map[string]prometheus.Gauge)
+	for _, reg := range registers {
+		metrics[reg.Name] = promauto.NewGauge(prometheus.GaugeOpts{
+			Name: setName(reg.Name),
+			Help: reg.Desc,
+		})
+	}
+
 	client := modbus.TCPClient(resourceAddress)
-	recordMetrics(client)
+	go recordMetrics(client, registers, metrics)
 
 	http.Handle(path, promhttp.Handler())
 
